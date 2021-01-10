68885bb6936563a9bc888e9add09366def1d4ebc
FriedrichKayak <jason.cochran@gmail.com>
2021-01-10 17:41:26 -0500

    added FAACIFP18 library and test project

---
+++ NASRTests/FAACIFP18Tests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NASRTests
+{
+    [TestClass]
+    public class FAACIFP18Tests
+    {
+        [TestMethod]
+        public void SampleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
