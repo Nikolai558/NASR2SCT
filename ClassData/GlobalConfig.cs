7ef7b8d6f113311f042a41754f3734b33acafcf2
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-05-04 16:22:56 -0600

    Trying to repackage Build
    Nothing has changed other than the version number.

--- ClassData/GlobalConfig.cs
+++ ClassData/GlobalConfig.cs
@@ -24,7 +24,7 @@ namespace NASARData
     /// </summary>
     public class GlobalConfig
     {
-        public static readonly string ProgramVersion = "0.7.8";
+        public static readonly string ProgramVersion = "0.7.9";
 
         public static string GithubVersion = "";
 
