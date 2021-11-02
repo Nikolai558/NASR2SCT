28cd5a273d159c3849c83a820a06cb95085f36ce
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-02 17:39:18 -0600

    Rename Project Folder

---
+++ NASR2SCT-WINFORMS/CreditsForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NASR_GUI
+{
+    public partial class CreditsForm : Form
+    {
+        public CreditsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void nikolasBolingLabel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Nikolai558");
+        }
+
+        private void kyleSandersLabel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/KSanders7070");
+        }
+
+        private void johnLewisLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
