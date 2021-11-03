8984b23d2e204514d7c6dbe45bf3984252b29c43
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2021-11-03 11:01:54 -0600

    Refactoring GetAptData
    Removed Unused Using statements.

--- NASR2SCT-DATA/DataAccess/GetAptData.cs
+++ NASR2SCT-DATA/DataAccess/GetAptData.cs
@@ -2,9 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Xml.Linq;
 using System.Xml.Serialization;
